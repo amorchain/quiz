ef2d68dfe49757aa78cb970d86a61d144f41402e
amorchain <amorchain@gmail.com>
2015-06-24 13:07:25 -0430

    Procfile

--- models/models.js
+++ models/models.js
@@ -9,18 +9,19 @@ var sequelize = new Sequelize(null, null, null,
 					);
 
 // Importar la Definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-exports.Quiz = Quiz // exportar definición de tabla Quiz
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function() {
+	// success ejecuta el manejador una vez creada la tabla
 	Quiz.count().success(function (count){
 		if(count === 0) { 		// la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia', 
 						  respuesta: 'Roma'
 						})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
