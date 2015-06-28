5643e3b56db45d02ead2b5c837b1a71d582c2d25
amorchain <amorchain@gmail.com>
2015-06-28 11:13:38 -0430

    despliegue del 28 de junio

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-var creditsController = require('../controllers/credits_controller');
+// var creditsController = require('../controllers/credits_controller');
+// router.get('/author', creditsController.credits);
 
-router.get('/author', creditsController.credits);
+// GET home page. 
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
 
 // Definición de rutas de /quizes
 router.get('/quizes', 			              quizController.index);
@@ -13,18 +20,18 @@ router.get('/quizes/:quizId(\\d+)',	          quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',    quizController.answer);
 
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'QUIZ' });
-});
 
-/* GET author. */
+// GET author
 router.get('/author', function (req, res, next) {
   res.render('author', {});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+/*
+router.get('/quizes/:quizId', quizController.question);
+router.get('/quizes/:quizId/answer', quizController.answer);
 
+// router.get('/quizes/question', quizController.question);
+// router.get('/quizes/answer', quizController.answer);
+*/
 
 module.exports = router;
